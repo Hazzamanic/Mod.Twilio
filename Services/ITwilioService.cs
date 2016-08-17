f289876e2a6e73b2ac21555b29392df5dd980f58
Hazza <harry@modstreaming.com>
2016-08-17 16:25:21 +0100

    twilio work

---
+++ Services/ITwilioService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard;
+using Twilio;
+
+namespace Mod.Twilio.Services {
+    public interface ITwilioService : IDependency {
+        void SendSms();
+    }
+
+    public class TwilioService : ITwilioService {
+
+
+        public void SendSms() {
+
+            TwilioRestClient smsClient = new TwilioRestClient(twilioSettings.AccountSID, twilioSettings.AuthToken);
+
+            try {
+                var msg = smsClient.SendSmsMessage(twilioSettings.FromNumber, number, context.MailMessage.Body);
+                Logger.Debug("Message sent to {0}: {1}", number, context.Type);
+            }
+            catch (Exception e) {
+                Logger.Error(e, "An unexpected error while sending a message to {0}: {1}", number, context.Type);
+            }
+        }
+    }
+}
